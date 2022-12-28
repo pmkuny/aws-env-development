5e923db58f71af2dd6b4e536b5caba0dba49b55f
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2022-12-28 01:22:44 +0000

    Fix and add in Cloud9

--- stacks/cloud9/cloud9.py
+++ stacks/cloud9/cloud9.py
@@ -1,24 +1,39 @@
-from aws_cdk import aws_cloud9_alpha as c9
+import aws_cdk as cdk
+from aws_cdk import aws_cloud9 as cloud9
 from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ssm as ssm
 
 from constructs import Construct
 from aws_cdk import Stack
+from stacks.helper_functions import get_user_arn_from_iam, get_user_arn_from_ssm
+from stacks.helper_functions import get_user_arn_from_ssm
 
 
 class Cloud9EnvStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Calls a helper function to get the correct ARN for Cloud9 ownership
+        current_arn = get_user_arn_from_ssm(self)
+        print(current_arn)
     
+        # Create our VPC to host our EC2 instance
+        # Default Subnet Configurations here will give extra uneeded subnets.
         vpc = ec2.Vpc(
             self,
             "VPC",
         )
-
-
-        c9env = c9.Ec2Environment(
-               self,
-               "Cloud9Env",
-               vpc=vpc,
-               image_id=c9.ImageId.AMAZON_LINUX_2
-               )
+        
+        # Create a Cloud9 instance that ties to our SSO-assumed role (taken from Parameter Store)
+        self.dev_env = cloud9.CfnEnvironmentEC2(
+            self,
+            "DevEnvironment",
+            automatic_stop_time_minutes=30,
+            connection_type='CONNECT_SSM',
+            description="Development Environment",
+            image_id="amazonlinux-2-x86_64",
+            instance_type=ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM).to_string(),
+            name="Dev Environment",
+            owner_arn=current_arn,
+            subnet_id=vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS).subnet_ids[0]
+            )
--- stacks/helper_functions.py
+++ stacks/helper_functions.py
@@ -1,5 +1,17 @@
 from aws_cdk import aws_ssm as ssm
+from aws_cdk import aws_iam as iam
+
 def get_ip(calling_stack):
     my_ip = ssm.StringParameter
 
     return f'{my_ip.value_for_string_parameter(calling_stack, parameter_name="/cdk/global/ip")}/32'
+    
+def get_user_arn_from_ssm(calling_stack):
+    my_arn = ssm.StringParameter
+    
+    return f'{my_arn.value_for_string_parameter(calling_stack, parameter_name="/cdk/global/assumed_arn")}'
+
+def get_user_arn_from_iam(calling_stack):
+    my_iam_role = iam.Role.from_role_name(calling_stack, "SSOReservedRole", role_name="AWSReservedSSO_AWSAdministratorAccess_03b9350052ec1191")
+    print(my_iam_role.role_arn)
+    return my_iam_role.role_arn
